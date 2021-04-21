8a9bf0ff048b0455fd55aa1824bbd4f5967e3575
김정환 <ej88ej@gmail.com>
2021-04-21 10:29:19 +0900

    react/search-form-2 구현

--- 2-react/js/main.js
+++ 2-react/js/main.js
@@ -1,17 +1,19 @@
-// TODO
-const element = (
-  <>
-    <header>
-      <h2 className="container">검색</h2>
-    </header>
-    <div className="container">
-      <form>
-        <input type="text" placeholder="검색어를 입력하세요" autoFocus />
-        <button type="reset" class="btn-reset"></button>
-      </form>
-    </div>
-  </>
-);
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <header>
+          <h2 className="container">검색</h2>
+        </header>
+        <div className="container">
+          <form>
+            <input type="text" placeholder="검색어를 입력하세요" autoFocus />
+            <button type="reset" class="btn-reset"></button>
+          </form>
+        </div>
+      </>
+    );
+  }
+}
 
-// TODO
-ReactDOM.render(element, document.querySelector("#app"));
+ReactDOM.render(<App />, document.querySelector("#app"));
