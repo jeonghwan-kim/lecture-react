ba9dd321347ed31ff5b151b2c798145a56cc67a4
김정환 <ej88ej@gmail.com>
2021-04-21 18:29:13 +0900

    query -> searchKeyword 변경

--- 2-react/js/main.js
+++ 2-react/js/main.js
@@ -63,7 +63,7 @@ class App extends React.Component {
           type="text"
           placeholder="검색어를 입력하세요"
           autoFocus
-          value={this.state.query}
+          value={this.state.searchKeyword}
           onChange={(event) => this.handleChangeInput(event)}
         />
         {this.state.searchKeyword.length > 0 && (
