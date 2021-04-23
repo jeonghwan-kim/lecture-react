3f660c92954db8cc59d7538d69f8afc02f489ab4
김정환 <ej88ej@gmail.com>
2021-04-23 10:23:18 +0900

    component/scaffolding

---
+++ 3-component/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    main: "./src/main.js",
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  plugins: [new CleanWebpackPlugin()],
+};
