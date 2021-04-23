c7def4587ae1d25aae7107664dffcf015f4f32b1
김정환 <ej88ej@gmail.com>
2021-04-23 11:31:10 +0900

    component/header-1 구현

--- 3-component/src/App.js
+++ 3-component/src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
+import Header from "./components/Header.js";
 
 export default class App extends React.Component {
   render() {
-    // TODO
-    return <>TODO: App 컴포넌트</>;
+    return <Header />;
   }
 }
---
+++ 3-component/src/components/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header = () => (
+  <header>
+    <h2 className="container">검색</h2>
+  </header>
+);
+
+export default Header;
