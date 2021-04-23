34df2dba83ec99d7b7fdee9183e4823d8209df32
김정환 <ej88ej@gmail.com>
2021-04-23 11:33:12 +0900

    component/header-2 TODO 추가

--- 3-component/src/App.js
+++ 3-component/src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/Header.js";
 
 export default class App extends React.Component {
   render() {
+    // TODO
     return <Header />;
   }
 }
--- 3-component/src/components/Header.js
+++ 3-component/src/components/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+// TODO
 const Header = () => (
   <header>
     <h2 className="container">검색</h2>
