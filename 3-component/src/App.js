dcf45e1bacca22dd816166832e13c6d0e7b788af
김정환 <ej88ej@gmail.com>
2021-04-23 10:44:53 +0900

    component/app 구현

---
+++ 3-component/src/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default class App extends React.Component {
+  render() {
+    return <>TODO: App 컴포넌트</>;
+  }
+}
--- 3-component/src/main.js
+++ 3-component/src/main.js
@@ -1,1 +1,5 @@
-console.log("main.js");
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App.js";
+
+ReactDOM.render(<App />, document.querySelector("#app"));
