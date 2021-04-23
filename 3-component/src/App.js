d92ccf68996acca42104c4f86140c39aac0c832d
김정환 <ej88ej@gmail.com>
2021-04-23 13:06:02 +0900

    component/search-form-1 구현

--- 3-component/src/App.js
+++ 3-component/src/App.js
@@ -3,6 +3,13 @@ import Header from "./components/Header.js";
 
 export default class App extends React.Component {
   render() {
-    return <Header title="검색" />;
+    return (
+      <>
+        <Header title="검색" />
+        <div className="container">
+          <SearchForm />
+        </div>
+      </>
+    );
   }
 }
---
+++ 3-component/src/components/SearchForm.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+export default class SearchForm extends React.Component {
+  constructor() {
+    super();
+
+    this.state = { searchKeyword: "" };
+  }
+
+  handleChangeInput(event) {
+    const searchKeyword = event.target.value;
+    this.setState({ searchKeyword });
+  }
+
+  render() {
+    const { searchKeyword } = this.state;
+
+    return (
+      <form>
+        <input
+          type="text"
+          placeholder="검색어를 입력하세요"
+          autoFocus
+          value={searchKeyword}
+          onChange={(event) => this.handleChangeInput(event)}
+        />
+        {searchKeyword.length > 0 && (
+          <button type="reset" className="btn-reset" />
+        )}
+      </form>
+    );
+  }
+}
