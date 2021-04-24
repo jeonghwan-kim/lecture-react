3b115de66018369385fefce29e28215ab1be841d
김정환 <ej88ej@gmail.com>
2021-04-24 16:35:48 +0900

    component/composition-2 TODO 추가

--- 3-component/src/components/HistoryList.js
+++ 3-component/src/components/HistoryList.js
@@ -1,51 +1,34 @@
 import React from "react";
 import { formatRelativeDate } from "../helpers.js";
 import store from "../Store.js";
-import List from "./List.js";
+import { List } from "./List.js";
 
-export default class HistoryList extends React.Component {
-  constructor() {
-    super();
+// export default class HistoryList extends List {
+//   componentDidMount() {
+//     this.fetch();
+//   }
 
-    this.state = {
-      HistoryList: [],
-    };
-  }
+//   fetch() {
+//     const data = store.getHistoryList();
+//     this.setState({ data });
+//   }
 
-  componentDidMount() {
-    this.fetch();
-  }
+//   handleClickRemove(event, keyword) {
+//     event.stopPropagation();
+//     store.removeHistory(keyword);
+//     this.fetch();
+//   }
 
-  fetch() {
-    const historyList = store.getHistoryList();
-    this.setState({ historyList });
-  }
-
-  handleClickRemove(event, keyword) {
-    event.stopPropagation();
-    store.removeHistory(keyword);
-    this.fetch();
-  }
-
-  render() {
-    const { onClick } = this.props;
-    const { historyList } = this.state;
-
-    return (
-      <List
-        data={historyList}
-        onClick={onClick}
-        renderItem={(item) => (
-          <>
-            <span>{item.keyword}</span>
-            <span className="date">{formatRelativeDate(item.date)}</span>
-            <button
-              className="btn-remove"
-              onClick={(event) => this.handleClickRemove(event, item.keyword)}
-            />
-          </>
-        )}
-      />
-    );
-  }
-}
+//   renderItem(item) {
+//     return (
+//       <>
+//         <span>{item.keyword}</span>
+//         <span className="date">{formatRelativeDate(item.date)}</span>
+//         <button
+//           className="btn-remove"
+//           onClick={(event) => this.handleClickRemove(event, item.keyword)}
+//         />
+//       </>
+//     );
+//   }
+// }
