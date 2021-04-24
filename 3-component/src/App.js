a5180ae064868bc09860e98ca32e02076b72f0ab
김정환 <ej88ej@gmail.com>
2021-04-24 14:03:16 +0900

    component/search-form-1 SearchForm 모듈 가져오기

--- 3-component/src/App.js
+++ 3-component/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "./components/Header.js";
+import SearchForm from "./components/SearchForm.js";
 
 export default class App extends React.Component {
   search() {
