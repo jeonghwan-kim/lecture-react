8ba5aba3f382f1b13883fb5491e159ed471627bc
김정환 <ej88ej@gmail.com>
2021-08-22 14:28:28 +0900

    오타수정: dispaly -> display

--- 1-vanilla/js/views/View.js
+++ 1-vanilla/js/views/View.js
@@ -9,7 +9,7 @@ export default class View {
     if (!element) throw "no element";
 
     this.element = element;
-    this.originalDisplay = this.element.style.dispaly || "";
+    this.originalDisplay = this.element.style.display || "";
 
     return this;
   }
