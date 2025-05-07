c6c8ec328674774e8298abcc96ca0f598bc63e21
chaerimjo-web <chaerim010710@gmail.com>
2025-05-07 15:01:49 +0900

    버튼 조건 설정

--- 1-vanilla/js/views/SearchFormView.js
+++ 1-vanilla/js/views/SearchFormView.js
@@ -1,7 +1,14 @@
-import View from "./View";
+import { qs } from "../helpers.js";
+import View from "./View.js";
 
 export default class SearchFormView extends View {
   constructor() {
     super(qs("#search-form-view"));
+
+    this.resetElement = qs("[type=reset]", this.element);
+    this.showResetButton(false);
+  }
+  showResetButton(visible = true) {
+    this.resetElement.style.display = visible ? "block" : "none";
   }
 }
